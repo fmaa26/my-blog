32512437d909b37193ca81023786b0c02a328760
company <rookaa20149@gmail.com>
2022-05-17 08:00:46 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let nav = document.querySelector("nav.navbar"),
+  menuBtn = document.querySelector(".phone-menu");
+  menu = document.querySelector(".navbar .navbar__container>ul");
+
+document.addEventListener("scroll", (e) => {
+
+    let pageScroll = document.scrollingElement.scrollTop,
+    navPosition = nav.offsetTop;
+
+    
+    if (pageScroll > navPosition + 0.5) {
+        nav.classList.add("navbar--show"); //to add scrolled nav style
+    } else {
+        nav.classList.remove("navbar--show"); //to remove scrolled nav style
+    }
+});
+
+menuBtn.addEventListener("click", function(){
+    menu.classList.toggle("show-list");
+})
